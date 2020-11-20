bd07d76df1b4e30fcf68b0b6177a321a176f3b1c
Daniel TIllero <danieltillero67@gmail.com>
2020-11-20 18:05:11 -0500

    AWS S3 Integration

---
+++ backend/routes/api/songs.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { check } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+import { multipleMulterUpload, multiplePublicFileUpload } from "../../awsS3";
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+router.post(
+    "/",
+    multipleMulterUpload(["image", "Audio"]),
+    asyncHandler(async (req, res) => {
+        const songData = req.body;
+        userData.image = await multiplePublicFileUpload([req.img, req.audio]);
+        const song = new song(songData);
+        await user.save();
+        res.json(song);
+    })
+);
